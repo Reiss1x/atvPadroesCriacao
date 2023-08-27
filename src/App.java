c1b5226b3f0d18b728a3fdf9d31058fca05bfe5c
unknown <gabrielreis.reis1903@gmail.com>
2023-08-27 15:56:34 -0300

    Branch da terceira atividade construída e testada

--- src/App.java
+++ src/App.java
@@ -1,16 +1,10 @@
-import java.util.Collection;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Collection<String> coleçãoOrdenada = CollectionFactorySingleton.getInstance().createCollection(CollType.TAMANHO_CONHECIDO_ORDENADO);
-
-
-        coleçãoOrdenada.add("banana");
-        coleçãoOrdenada.add("morango");
-        coleçãoOrdenada.add("maça");
-        for(String s : coleçãoOrdenada){
-            System.out.println(s);
+        for(int x = 0; x<=5;x++){
+            Personagem personagemAleatorio = Personagem.criarPersonagemAleatorio();
+            System.out.println(personagemAleatorio);
         }
     }
 }
